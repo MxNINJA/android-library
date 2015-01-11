954435710f8fc2947d19c4a307c7bd81cfb00544
mnajjar <mxninja-@hotmail.com>
2015-01-11 15:56:14 +0200

    push

---
+++ library/src/main/java/org/mx/library/MxNINJA.java
@@ -0,0 +1,12 @@
+package org.mx.library;
+
+/**
+ * @author Mohammad Al-Najjar
+ * @since 11/01/2015
+ */
+public class MxNINJA {
+
+    public static String test(String a){
+        return a;
+    }
+}
